46371913028ba5943d504f3ac3d8bd9c8da23cb5
[HanumanJat8698] <[hanuman.jat@intimetec.com]>
2019-03-06 16:17:33 +0530

    switch added

--- Cal.java
+++ Cal.java
@@ -1,19 +1,32 @@
+import java.util.*;
 class Cal
 {
+	
 	public static void main(String args[])
 	{
+		Scanner sc=new Scanner(System.in);
 		while(true)
 		{
-			System.out.println("Number 10 and 20");
-			int a=10;
-			int b=20;
-			System.out.println("Addition :"+(a+b));
-			System.out.println("Subtraction :"+(a-b));
-			System.out.println("Multiplication :"+(a*b));
-			System.out.println("Division :"+(a/b));
-			
-			System.exit(0);
-			
+			System.out.println("Enter 2 numbers");
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			System.out.println("Enter choice \n 1.ad\n2.sub\n3.mul\n4.div\n");
+			int c=sc.nextInt();
+			switch(c)
+			{
+			case 1:	System.out.println("Addition :"+(a+b));
+					break;
+			case 2:	System.out.println("Subtraction :"+(a-b));
+					break;
+			case 3:	System.out.println("Multiplication :"+(a*b));
+					break;
+			case 4:	System.out.println("Division :"+(a/b));
+					break;
+			case 5:System.out.println("Exiting..");
+					System.exit(0);
+			default:System.out.println("Wrong choice");
+					break;
+			}
 		}
 	}
 }
